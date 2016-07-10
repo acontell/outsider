243bf1757430aaa1e7cb6ba3b9c1644b7707c976
acontell <arcadio@debi>
2016-07-10 17:27:35 +0200

    Added functionality for GET all users test

--- db/controllers/user.js
+++ db/controllers/user.js
@@ -1,5 +1,12 @@
-//var mongoose = require('mongoose'),
-//    User = require('../models/user');
+'use strict';
 
-module.exports = function () {
-};
+var UserModel = require('../models/user'),
+    api = {
+        getAll: function (req, res) {
+            return UserModel.find(function (err, users) {
+                return res.json(err ? err : users);
+            });
+        }
+    };
+
+module.exports = api;
---
+++ test/helper.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var _ = require('lodash'),
+        chai = require('chai'),
+        helper = {
+            chai: chai,
+            testProperties: function (obj1, obj2) {
+                _.each(obj2, function (value, key) {
+                    obj1.should.have.property(key);
+                    obj1[key].should.equal(value);
+                });
+            },
+            setEnvAndDropModel: function (env, model, callback) {
+                process.env.NODE_ENV = env;
+                model.collection.drop();
+                callback();
+            }
+        };
+
+require('should-http');
+helper.chai.use(require('chai-http')).should();
+
+module.exports = helper;
