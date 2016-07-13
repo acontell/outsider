0bcf7c9befc27699e88f8709497c45b43ff18c53
acontell <arcadio@debi>
2016-07-13 18:21:17 +0200

    Added process.cwd()

--- test/frontend/features/support/hooks.js
+++ test/frontend/features/support/hooks.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var config = require('../../../../config'),
+var config = require(process.cwd() + '/config'),
     hooks = function () {
         this.Before(function () {
             this.driver.get(config.testServerURI);
